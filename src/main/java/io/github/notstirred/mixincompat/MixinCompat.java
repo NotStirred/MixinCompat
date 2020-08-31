b4f3e3e084156ce298ad113f0fa1424dafe0b078
NotStirred <notstirred9@gmail.com>
2020-08-31 21:01:14 +0100

    Initial commit - Mod works!

---
+++ src/main/java/io/github/notstirred/mixincompat/MixinCompat.java
@@ -0,0 +1,14 @@
+package io.github.notstirred.mixincompat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = MixinCompat.MOD_ID,
+        name = MixinCompat.MOD_NAME,
+        version = MixinCompat.VERSION
+)
+public class MixinCompat {
+    public static final String MOD_ID = "mixincompat";
+    public static final String MOD_NAME = "Mixin Compatibility";
+    public static final String VERSION = "1.0-SNAPSHOT";
+}
